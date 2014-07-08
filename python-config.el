410b28f88e8333b390290db1a40ef0ad801b4e1d
Gérôme Chardon <gerome.chardon@gmail.com>
2014-07-08 17:30:17 +0200

    update python hook

--- python-config.el
+++ python-config.el
@@ -1,5 +1,8 @@
 ;; Start autopair to complete brackets and quotes
-;(add-hook 'python-mode-hook 'autopair-mode)
+(add-hook 'python-mode-hook 'autopair-mode)
 
 ;; Delete trailing whitespace when saving (compliance with PEP8)
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; Python hook to Highlight TODO, FIXME, ...
+(add-hook 'python-mode-hook 'turn-on-fic-mode)
