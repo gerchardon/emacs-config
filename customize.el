3cb7698878ef35bd524b263df66dcad829f642f3
Gérôme Chardon <gerome.chardon@gmail.com>
2014-10-13 10:13:46 +0200

    Disable *..* buffer for next and prev function

--- customize.el
+++ customize.el
@@ -14,6 +14,30 @@
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
 
+
+;;
+;; Next and previous buffer ignore *...* buffer
+;;
+(defadvice next-buffer (after avoid-messages-buffer-in-next-buffer)
+  "Advice around `next-buffer' to avoid going into the *Messages* buffer."
+  ;;(when (string= "*Messages*" (buffer-name))
+  (when (string-match "\\*.*\\*" (buffer-name))
+    (next-buffer)))
+
+;; activate the advice
+(ad-activate 'next-buffer)
+
+
+(defadvice previous-buffer (after avoid-messages-buffer-in-next-buffer)
+  "Advice around `previous-buffer' to avoid going into the *Messages* buffer."
+  ;;(when (string= "*Messages*" (buffer-name))
+  (when (string-match "\\*.*\\*" (buffer-name))
+    (previous-buffer)))
+
+;; activate the advice
+(ad-activate 'previous-buffer)
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
