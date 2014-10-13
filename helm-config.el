8936e975a5f2c494f3c194ed28ca594d6c0480b6
Gérôme Chardon <gerome.chardon@gmail.com>
2014-10-13 10:14:36 +0200

    Helm configuration

--- helm-config.el
+++ helm-config.el
@@ -1,1 +1,21 @@
+(setq helm-quick-update                     t ; do not display invisible candidates
+      helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-buffers-fuzzy-matching           t ; fuzzy matching buffer names when non--nil
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t)
+
+(setq enable-recursive-minibuffers t)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "C-x b") 'helm-mini)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+
+
+;; (setq helm-boring-buffer-regexp-list '())
+(setq helm-boring-buffer-regexp-list '("\\*.*\\*"))
+(setq helm-find-ignored-directories '(".venv"))
 (helm-mode 1)
+
+;; (setq enable-recursive-minibuffers t)
+(setq helm-grep-in-recurse t)
