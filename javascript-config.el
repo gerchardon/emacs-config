124fdaf83df58821e91f8a440c1de828c8dd71eb
Gérôme Chardon <gerome.chardon@gmail.com>
2014-12-21 00:25:59 +0100

    Change javascript indent line

--- javascript-config.el
+++ javascript-config.el
@@ -1,13 +1,13 @@
 ;; Javascript config
 
-(setq js-indent-level 4)
-(setq c-basic-offset 4)
+(setq js-indent-level 2)
+(setq c-basic-offset 2)
 
 (setq js3-consistent-level-indent-inner-bracket t)
-(setq js3-paren-indent-offset 4)
+(setq js3-paren-indent-offset 2)
 (custom-set-variables
  '(js3-lazy-semicolons t)
- '(js3-paren-indent-offset 4))
+ '(js3-paren-indent-offset 2))
 
 ;; Start autopair to complete brackets and quotes
 (add-hook 'js-mode-hook 'autopair-mode)
