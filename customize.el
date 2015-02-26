9402e914a2e85a042ad67e51dcb6ab9ae6181c51
Gérôme Chardon <gerome.chardon@gmail.com>
2015-02-26 21:11:34 +0100

    Ask when closing emacs

--- customize.el
+++ customize.el
@@ -37,6 +37,18 @@
 ;; activate the advice
 (ad-activate 'previous-buffer)
 
+;; ask before close emacs
+(defun ask-before-closing ()
+  "Ask whether or not to close, and then close if y was pressed"
+  (interactive)
+  (if (y-or-n-p (format "Are you sure you want to exit Emacs? "))
+      (if (< emacs-major-version 22)
+          (save-buffers-kill-terminal)
+        (save-buffers-kill-emacs))
+    (message "Canceled exit")))
+
+(when window-system
+  (global-set-key (kbd "C-x C-c") 'ask-before-closing))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
