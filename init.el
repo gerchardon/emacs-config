cbeb9b704277361484363e0db27ac9eba63285cd
Gérôme Chardon <gerome.chardon@gmail.com>
2015-06-28 17:01:47 +0200

    update

--- init.el
+++ init.el
@@ -1,14 +1,12 @@
 ;; Emacs Config
 
-(add-to-list 'load-path ".emacs.d")
-
 ;; Init All Plugin with Cask & Pallet
 (require 'cask "~/.cask/cask.el")
 
 
-(load "proxy.el")
-(load "pre-cask.el")
-(load "customize.el")
+(load-file "~/.emacs.d/proxy.el")
+(load-file "~/.emacs.d/pre-cask.el")
+(load-file "~/.emacs.d/customize.el")
 (cask-initialize)
 
 ;; Load all files with *-config.el
