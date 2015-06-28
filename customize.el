6980000a01c6433b333c8c56b3022a5d3ea5a77a
Gérôme Chardon <gerome.chardon@gmail.com>
2015-06-28 17:59:33 +0200

    Change font

--- customize.el
+++ customize.el
@@ -73,4 +73,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Ubuntu Mono" :foundry "unknown" :slant normal :weight normal :height 113 :width normal)))))
+ '(default ((t (:family "Ubuntu Mono" :foundry "unknown" :slant normal :weight normal :height 80 :width normal)))))
