adf03c1220c0b2eae268d39869ce98d7278af803
Gérôme Chardon <gerome.chardon@gmail.com>
2015-07-06 21:32:14 +0200

    Upgrade magit to 1.4.0

--- magit-config.el
+++ magit-config.el
@@ -4,6 +4,9 @@
 (setq magit-remote-ref-format 'remote-slash-branch)
 (setq magit-completing-read-function 'magit-iswitchb-completing-read)
 (setq magit-save-some-buffers nil)
+; magit 1.4.0 (no revert before git)
+(setq magit-auto-revert-mode nil)
+(setq magit-last-seen-setup-instructions "1.4.0")
 
 (global-set-key (kbd "C-x g") 'magit-status)
 
