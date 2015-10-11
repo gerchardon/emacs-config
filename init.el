963788644fe55023cb1ce8d12e8be26b94a1e7a8
Gérôme Chardon <gerome.chardon@gmail.com>
2015-10-11 17:25:53 +0200

    Add revert buffer to F5

--- init.el
+++ init.el
@@ -23,20 +23,4 @@
 ;; ShortCuts
 ;;;;;;;;;;;;
 (global-set-key (kbd "C-x k") 'kill-this-buffer)
-
-;;;
-;; C-x g => Magit-Status
-;;
-;;
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (package-build shut-up epl git commander f dash s))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(global-set-key (kbd "<f5>") 'revert-buffer)
