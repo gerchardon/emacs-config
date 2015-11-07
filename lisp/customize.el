07093ffe23ab26505a8b6befe03be277d58698a7
Gérôme Chardon <gerchardon@users.noreply.github.com>
2015-11-07 14:58:39 +0100

    Update dired ignore files

--- lisp/customize.el
+++ lisp/customize.el
@@ -21,7 +21,7 @@
 (require 'dired-x)
 (setq-default dired-omit-files-p t) ; this is buffer-local variable
 (setq dired-omit-files
-    (concat dired-omit-files "\\|^\\..+$\\|\\.pyc$|\\.elc$"))
+    (concat dired-omit-files "\\|^\\..+$\\|\\.pyc$|\\.elc$|^\\.tern-port$"))
 (setq dired-recursive-deletes 'always)
 
 ;;
