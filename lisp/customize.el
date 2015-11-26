9b521975e8ec7c63ffef4192fcf665561e224956
Gérôme Chardon <gerchardon@users.noreply.github.com>
2015-11-26 16:59:25 +0100

    Add zoom in/out globaly

--- lisp/customize.el
+++ lisp/customize.el
@@ -24,6 +24,27 @@
     (concat dired-omit-files "\\|^\\..+$\\|\\.pyc$|\\.elc$|^\\.tern-port$"))
 (setq dired-recursive-deletes 'always)
 
+;; Zoom in/out globaly
+(define-globalized-minor-mode
+  global-text-scale-mode
+  text-scale-mode
+  (lambda () (text-scale-mode 1)))
+
+(defun global-text-scale-adjust (inc) (interactive)
+  (text-scale-set 1)
+  (kill-local-variable 'text-scale-mode-amount)
+  (setq-default text-scale-mode-amount (+ text-scale-mode-amount inc))
+  (global-text-scale-mode 1))
+
+(global-set-key (kbd "M-0")
+  '(lambda () (interactive)
+     (global-text-scale-adjust (- text-scale-mode-amount))
+     (global-text-scale-mode -1)))
+(global-set-key (kbd "M-+")
+  '(lambda () (interactive) (global-text-scale-adjust 1)))
+(global-set-key (kbd "M--")
+  '(lambda () (interactive) (global-text-scale-adjust -1)))
+
 ;;
 ;; Next and previous buffer ignore *...* buffer
 ;;
